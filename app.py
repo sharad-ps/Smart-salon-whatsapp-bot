717daa75ebb8370d8a96356cf06b725505607536
sharad-ps <amytab83@gmail.com>
2026-02-08 10:08:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1089,4 +1089,6 @@ def health():
     return jsonify({'status': 'healthy'}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT",5000))
+    app.run(host='0.0.0.0', port=port)
